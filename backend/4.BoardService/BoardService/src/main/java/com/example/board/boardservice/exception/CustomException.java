8075dea7d911ba5aa96e92ab53ae40eb9fedded8
YeongJae0114 <yjl8628@naver.com>
2024-11-14 17:39:14 +0900

    CustomException, ErrorCode 수정

--- backend/4.BoardService/BoardService/src/main/java/com/example/board/boardservice/exception/CustomException.java
+++ backend/4.BoardService/BoardService/src/main/java/com/example/board/boardservice/exception/CustomException.java
@@ -1,17 +1,32 @@
 package com.example.board.boardservice.exception;
 
 import com.example.board.boardservice.response.model.ErrorCode;
+import java.util.AbstractMap;
+import java.util.Map;
 import lombok.Getter;
+import org.springframework.util.StringUtils;
 
 @Getter
 public class CustomException extends RuntimeException {
+    @Getter
     private final ErrorCode errorCode;
-    private final Object data;
+    private String message;
+    @Getter
+    private Map.Entry<String, Object> data;
 
+    // 생성자
     public CustomException(ErrorCode errorCode, String message, Object data) {
-        super(message);
         this.errorCode = errorCode;
-        this.data = data;
+        this.message = message;
+        this.data = new AbstractMap.SimpleEntry<>(data.getClass().getSimpleName(), data);
+    }
+
+    public String getMessage(){
+        // CustomException으로 넘어오는 message가 있으면 그걸 쓰고, 없으면, errorCode의 기본 메세지를 리턴한다.
+        if(StringUtils.hasLength(this.message)) {
+            return this.message;
+        }
+        return errorCode.getMessage();
     }
 
 }
--- backend/4.BoardService/BoardService/src/main/java/com/example/board/boardservice/response/model/ErrorCode.java
+++ backend/4.BoardService/BoardService/src/main/java/com/example/board/boardservice/response/model/ErrorCode.java
@@ -1,29 +1,24 @@
 package com.example.board.boardservice.response.model;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
 
-    // 성공 코드
-    OK(2000, "OK"),
-
-    // 클라이언트 오류
-    BAD_REQUEST(4000, "Bad Request"),
-    UNAUTHORIZED(4010, "Unauthorized"),
-    FORBIDDEN(4030, "Forbidden"),
-    NOT_FOUND(4040, "Resource Not Found"),
-
-    // 서버 오류
-    INTERNAL_SERVER_ERROR(5000, "Internal Server Error"),
-    SERVICE_UNAVAILABLE(5030, "Service Unavailable");
+    OK(2000, "OK", HttpStatus.OK),
+    SERVER_ERROR(5001, "SERVER ERROR!", HttpStatus.INTERNAL_SERVER_ERROR),
+    BAD_REQUEST(500, "BAD REQUEST", HttpStatus.OK)
+    ;
 
     private final int code;
     private final String message;
+    private final HttpStatus httpStatus;
 
-    ErrorCode(int code, String message) {
+    ErrorCode(int code, String message, HttpStatus httpStatus) {
         this.code = code;
         this.message = message;
+        this.httpStatus = httpStatus;
     }
 
 }
