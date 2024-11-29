4ecdd9a8e6cb5ad97df08cede42a1ae337b24cd8
YeongJae0114 <yjl8628@naver.com>
2024-11-29 23:45:39 +0900

    WebConfig에 Swagger, HibernateConfig 통합

--- backend/4.BoardService/BoardService/src/main/java/com/example/board/boardservice/config/WebConfig.java
+++ backend/4.BoardService/BoardService/src/main/java/com/example/board/boardservice/config/WebConfig.java
@@ -1,9 +1,15 @@
 package com.example.board.boardservice.config;
 
 import com.example.board.boardservice.log.LoggingInterceptor;
+import com.example.board.boardservice.log.QueryCountInspector;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.cfg.AvailableSettings;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,13 +24,27 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**");
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(false)
-                .maxAge(3600);
+    //swagger
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("CodeArena Swagger")
+                .description("CodeArena 유저 및 인증 , ps, 알림에 관한 REST API")
+                .version("1.0.0");
+    }
+
+    // HibernateConfig log 추적기
+    private final QueryCountInspector queryCountInspector;
+
+    @Bean
+    public HibernatePropertiesCustomizer configureStatementInspector() {
+        return hibernateProperties ->
+                hibernateProperties.put(AvailableSettings.STATEMENT_INSPECTOR, queryCountInspector);
     }
 }
