38d17f2de06b6746dd423c033391ebb345315e1b
YeongJae0114 <yjl8628@naver.com>
2024-12-02 12:41:38 +0900

    update

--- backend/4.BoardService/BoardService/src/main/java/com/example/board/boardservice/entity/Comment.java
+++ backend/4.BoardService/BoardService/src/main/java/com/example/board/boardservice/entity/Comment.java
@@ -1,4 +1,13 @@
 package com.example.board.boardservice.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
 public class Comment {
+    @Id @GeneratedValue
+    private Long id;
 }
