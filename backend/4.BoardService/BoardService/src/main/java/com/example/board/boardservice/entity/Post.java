84f4682b8f1bdf5ab66efcabb5b3d564e4dee1d9
YeongJae0114 <yjl8628@naver.com>
2024-12-02 12:40:58 +0900

    Builder 로 객체 생성

--- backend/4.BoardService/BoardService/src/main/java/com/example/board/boardservice/entity/Post.java
+++ backend/4.BoardService/BoardService/src/main/java/com/example/board/boardservice/entity/Post.java
@@ -3,18 +3,28 @@ package com.example.board.boardservice.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Setter @Getter
+@Getter
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
     @Id @GeneratedValue
     private Long id;
 
     private String title;
     private String content;
-    private String author;
     private LocalDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User author;
 }
